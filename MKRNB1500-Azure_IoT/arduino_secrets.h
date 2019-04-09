fdec18d244b7b174ef45bdd9fe4425adb76ea80b
Matt Sinclair <15816036+mjksinc@users.noreply.github.com>
2019-04-09 23:56:39 +1000

    Example arduino_secrets
    Example arduino_secrets

---
+++ MKRNB1500-Azure_IoT/arduino_secrets.h
@@ -0,0 +1,8 @@
+// NB settings
+#define SECRET_PINNUMBER ""
+
+// Fill in the hostname of your Azure IoT Hub broker
+#define SECRET_BROKER    "<IoT Hub Host Name>"
+
+// Fill in the Connection string for your device
+#define CONN_STRING "<IoT Hub Connection String>"
